8b101d7ff695ab081030cd92a162b1410199a3ef
Hanish <dm281972@gmail.com>
2024-10-02 01:20:47 +0530

    home page text added

--- app/page.js
+++ app/page.js
@@ -5,27 +5,34 @@ export default function Home() {
   return (
     <>
     <div className="bg-[#91ddea] relative">
-      {/* Empty space above the images */}
-      <div className="h-[35vh] md:h-[40vh] lg:h-[25vh]"></div>
+  {/* Empty space above the images */}
+  <div className="h-[35vh] md:h-[40vh] lg:h-[25vh]"></div>
   
-      {/* Logo Image - Full visibility on mobile, partially covered on larger screens */}
-      <img 
-        src="/LOGO (2).png" 
-        className="absolute inset-0 z-10 w-full h-[65vh] object-contain 
-        lg:h-[80vh] lg:top-[10%] md:top-[15%] sm:top-[20%] top-[25%]" 
-        alt="Logo"
-      />
-  
-      {/* College Image - Positioned in front of the logo on larger screens */}
-      <img 
-        src="/CLG.png" 
-        className="w-full object-cover h-[45vh] sm:h-[60vh] md:h-[70vh] lg:h-full z-20 relative lg:-mt-[20vh]" 
-        alt="College"
-      />
-    </div>
-  
-    {/* Footer or any section after */}
-    <div className="h-[30vh] w-full bg-black"></div>
+  {/* Title with Montserrat font */}
+  <div className="absolute top-10 left-0 right-0 flex flex-col items-center justify-center">
+    <h1 className="font-extrabold font-montserrat text-[#9a0107] text-1xl md:text-3xl lg:text-5xl">The voices of</h1>
+    <h1 className="font-extrabold font-montserrat text-[#9a0107] text-1xl md:text-3xl lg:text-5xl">Palestine Rise</h1>
+  </div>
+
+  {/* Logo Image - Full visibility on mobile, partially covered on larger screens */}
+  <img 
+    src="/LOGO (2).png" 
+    className="absolute inset-0 z-10 w-full h-[65vh] object-contain 
+    lg:h-[80vh] lg:top-[10%] md:top-[15%] sm:top-[20%] top-[25%]" 
+    alt="Logo"
+  />
+
+  {/* College Image - Positioned in front of the logo on larger screens */}
+  <img 
+    src="/CLG.png" 
+    className="w-full object-cover h-[45vh] sm:h-[60vh] md:h-[70vh] lg:h-full z-20 relative lg:-mt-[20vh]" 
+    alt="College"
+  />
+</div>
+
+{/* Footer or any section after */}
+<div className="h-[30vh] w-full bg-black"></div>
+
   </>
   
 
